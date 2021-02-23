0a106500e0d0d6786d3de0b5dddb9c6678366509
pavlinbl4@gmail.com <15deaDman>
2021-02-23 17:56:34 +0300

    Напишите программу, которая выводит число в стиле LCD калькулятора.
    На вход программе подаётся последовательность цифр, которую нужно вывести на экран в специальном стиле (см. пример).
    
    Размер всех цифр 4 символа в ширину и 7 символов в высоту. Между цифрами в выводе должен быть один пустой столбец. Перед первой цифрой не должно быть пробелов.
    
    Выведенные цифры должны быть обведены рамочкой, в углах которой находится символ x ("икс"), горизонтальная линия создаётся из символа - ("дефис"), а вертикальная -- из символа вертикальной черты: |.
    
    Формат ввода:
    Строка произвольной длины (минимум один символ), содержащая последовательность цифр.
    
    Формат вывода:
    9 строк, содержащих цифры, записанные в указанном в задании формате.

--- 5.2 Old LCD calculator.py
+++ 5.2 Old LCD calculator.py
@@ -1,1 +1,29 @@
 # https://stepik.org/lesson/21306/step/1?adaptive=true&unit=5109
+# n = input()
+lst = list(map( int,list(input())))
+
+
+line = " -- "
+rt = "   |"
+lt = "|   "
+tt = "|  |"
+ey = "    "
+# digits = input()
+digit = {0: [line, tt, tt, ey, tt, tt, line],
+         1: [ey, rt, rt, ey, rt, rt, ey],
+         2: [line, rt, rt, line, lt, lt, line],
+         3: [line, rt, rt, line, rt, rt, line],
+         4: [ey, tt, tt, line, rt,  rt, ey],
+         5: [line, lt, lt, line, rt, rt, line],
+         6: [line, lt, lt, line, tt,  tt, line],
+         7: [line, rt, rt, ey, rt, rt, ey],
+         8: [line, tt, tt, line, tt,  tt, line],
+         9: [line, tt, tt, line, rt,  rt, line]}
+
+print(f"x{'-' * (len(lst)*5-1)}x")
+for i in range(7):
+    print(f"|{digit[lst[0]][i]} {digit[lst[1]][i]} {digit[2][i]}"
+          f"{digit[3][i]} {digit[4][i]} {digit[5][i]}"
+          f"{digit[6][i]} {digit[7][i]} {digit[8][i]}"
+          f"{digit[9][i]}|")
+print(f"x{'-' * (len(lst)*5-1)}x")

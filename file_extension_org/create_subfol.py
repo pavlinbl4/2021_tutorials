b46a20bed51bc15bd5be9d62376192290193193f
pavlinbl4@gmail.com <15deaDman>
2021-04-03 16:47:36 +0300

    простая програмка для перемещения файлов в папке в сабфолдеры по типу разрешения файла

---
+++ file_extension_org/create_subfol.py
@@ -0,0 +1,28 @@
+"""
+программа создает сабфолдеры по расширениям файлов
+в заданной директории и переносит их туда
+"""
+import os
+import shutil
+
+
+def finder(path):
+    names = os.listdir(path)
+    for name in names:
+        fullname = os.path.join(path, name)
+        flag = True
+        if os.path.isfile(fullname) and name[0] != ".":
+            try:
+                os.makedirs(f'{path}/{name.split(".")[-1]}', exist_ok=True)  # создаю папку по имени расширения файла
+                shutil.move(fullname,
+                            f'{path}/{name.split(".")[-1]}/{name}')  # команда переносит файл в новую директорию
+                print(f'new folder created - {name.split(".")[-1]}')
+                flag = False
+            except FileExistsError:
+                print(f'problem with file {name}')  # при проблемах с файлами, пропустить их
+            finally:
+                continue
+    print('Not need in optimization' * flag)
+
+
+finder("/Users/evgeniy/Movies")

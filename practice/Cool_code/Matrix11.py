9d383bc164955a64910049399db5f62a2ce77e64
evgeniy <pavlinbl4@gmail.com>
2021-05-04 21:22:57 +0300

    матрицы 11

--- practice/Cool_code/Matrix11.py
+++ practice/Cool_code/Matrix11.py
@@ -21,3 +21,20 @@ for j in range(n):
     for i in range(m):
         print(arr[i][j], end = ' ')
         flag *= -1
+
+
+
+m = 5
+arr = [[i + j * m for i in range(m)] for j in range(m)]
+for i in arr:
+    print("\t".join(map(str, i)))
+print("=" * 50)
+
+for i in range(m):
+    print()
+    if i % 2 == 0:
+        for j in range(m):
+            print(arr[i][j], end=' ')
+    else:
+        for j in range(m - 1, -1, - 1):
+            print(arr[i][j], end=' ')
